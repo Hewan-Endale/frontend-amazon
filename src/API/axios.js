4aa2a0420ec35583554fe41efd1573c55ee9e973
Hewan-Endale <Hewanendale2@gmail.com>
2024-12-11 01:42:45 +0300

    trying

--- src/API/axios.js
+++ src/API/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   // backend deployed on render
   
   // baseURL: "http://localhost:5000",
-  baseURL: "https://amazon-backend-deploy-bk17.onrender.com",
+  baseURL: "https://amazon-backend-deploy-bk17.onrender.com"
 });
 
 export { axiosInstance };
